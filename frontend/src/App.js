4f0e4b66a933831f66a58992b5dc5659820af0a5
hamidbahram <hamidbahram2@gmail.com>
2020-04-09 13:08:13 +0430

    improve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 // import React from 'react';
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 // import axios from 'axios';
 
 class Counter extends Component {
   state = {
-    count: 0
-  }
-  
+    count: 0,
+  };
+
   // handelIncrement = (product) => {
   //     console.log(product);
   //     this.setState({ count: this.state.count + 1 });
@@ -22,24 +22,27 @@ class Counter extends Component {
   // };
 
   UpdateCounter = () => {
-    fetch('http://localhost:8000/add/1/')
-    .then((response) => response.json())
-    .then(counter => {
-      console.log(counter)
-      if (counter.new_object === this.state.count + 1){
-        this.setState({count : counter.new_object })
-      }
-    });
+    console.warn('UpdateCounter')
+    fetch("http://localhost:8000/add/1/")
+      .then((response) => response.json())
+      .then((counter) => {
+        console.log(counter);
+        if (counter.new_object === this.state.count + 1) {
+          this.setState({ count: counter.new_object });
+        }
+      });
   };
 
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/')
-    .then((response) => response.json())
-    .then(counter => {
-      this.setState({ count: counter[0].num });
-    });
+    console.warn('componentDidMount')
+    fetch("http://127.0.0.1:8000/api/")
+      .then((response) => response.json())
+      .then((counter) => {
+        console.log(counter)
+        this.setState({ count: counter[0].num });
+      });
   }
-  
+
   // componentDidUpdate(prevProps, prevState, snapshot) {
   //   if (this.state.count !== prevState.count){
   //     fetch('http://localhost:8000/show/1/')
@@ -50,16 +53,27 @@ class Counter extends Component {
   //   }
   // }
 
-render() {
-  return (
-    <div>
-      <p>
-        <span style={{fontSize:'30px'}} className={'badge badge-primary m-2'}>{this.state.count}</span> 
-      </p>
-      <button onClick={this.UpdateCounter} className={'btn btn-warning m-2'}>add</button>
-    </div>
-    )
+  render() {
+    console.warn('render')
+    return (
+      <div>
+        <p>
+          <span
+            style={{ fontSize: "30px" }}
+            className={"badge badge-primary m-2"}
+          >
+            {this.state.count}
+          </span>
+        </p>
+        <button 
+          onClick={this.UpdateCounter}
+          className={"btn btn-warning m-2"}
+        >
+          add
+        </button>
+      </div>
+    );
   }
 }
- 
+
 export default Counter;
